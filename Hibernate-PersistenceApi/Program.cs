9ab6c8bd1fc8bd2582189aab36591091a61b3873
zhuzhenguang <zgzhu@thoughtworks.com>
2018-03-15 12:50:22 +0800

    Add project files.

---
+++ Hibernate-PersistenceApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hibernate_PersistenceApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
